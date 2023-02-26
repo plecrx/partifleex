368b36c87633acacd4c18459ff82f6ee2760e3b9
Prescilla Lecurieux <prescilla.lecurieux@pretto.fr>
2023-02-26 23:39:28 +0100

    persist movies

--- src/features/movies/useMovies.ts
+++ src/features/movies/useMovies.ts
@@ -3,7 +3,7 @@ import { useEffect, useState } from 'react'
 import { Movie } from 'types/movie'
 
 export const useMovies = () => {
-  const [movies, setMovies] = useState<Movie[]>([])
+  const [movies, setMovies] = useState<Movie[]>(JSON.parse(window.localStorage.getItem('movies')))
   const [isLoading, setIsLoading] = useState<boolean>(true)
   const [isError, setIsError] = useState<boolean>(false)
 
@@ -32,8 +32,13 @@ export const useMovies = () => {
     const fetchMovies = async () => {
       await movies$
         .then((data) => {
-          setMovies(data as Movie[])
           setIsLoading(false)
+          const persistedMovies = JSON.parse(window.localStorage.getItem('movies'))
+          if (persistedMovies !== null) {
+            setMovies(persistedMovies)
+            return
+          }
+          setMovies(data as Movie[])
         })
         .catch(() => {
           setIsLoading(false)
@@ -42,6 +47,10 @@ export const useMovies = () => {
     }
     fetchMovies()
   }, [])
+
+  useEffect(() => {
+    window.localStorage.setItem('movies', JSON.stringify(movies))
+  }, [movies])
 
   return {
     addMovie,
